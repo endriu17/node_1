4f56122a2b430a65422b96b3dd2ce0311c691398
endriu17 <aolaszewski@gmail.com>
2018-08-31 17:26:22 +0200

    fix script js

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ process.stdin.on('readable', function() {
     var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.toString().trim();
-        if (instruction === '/exit') {
+        switch (instruction){
+            case '/exit':
             console.log('Your version of Node is: ' + process.versions.node + ' and the language of the system is: ' + process.env.lang + '. Congratulations!!!');
             process.stdout.write('Quitting app!\n');
             process.exit();
-        } else {
-            process.stderr.write('Wrong instruction!');
+        default:
+            process.stderr.write('Wrong instruction!\n');
         }
     }
 });
